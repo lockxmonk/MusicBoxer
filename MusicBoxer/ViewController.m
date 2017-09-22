c5ee5eb804da07bab7a1fefe539d75a60d7d8feb
lockxmonk <77249084@qq.com>
2017-09-22 19:29:16 +0800

    first commit

---
+++ MusicBoxer/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MusicBoxer
+//
+//  Created by 梁中豪 on 2017/9/22.
+//  Copyright © 2017年 梁中豪. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ MusicBoxerTests/MusicBoxerTests.m
@@ -0,0 +1,39 @@
+//
+//  MusicBoxerTests.m
+//  MusicBoxerTests
+//
+//  Created by 梁中豪 on 2017/9/22.
+//  Copyright © 2017年 梁中豪. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MusicBoxerTests : XCTestCase
+
+@end
+
+@implementation MusicBoxerTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
