7a5e21ca7849eea899eacd4fa8e4d10510d1ad76
lockxmonk <77249084@qq.com>
2017-09-25 17:06:34 +0800

    创建tab框架，建立两个视图 音乐和设置。

---
+++ MusicBoxer/Controllers/MBTabBarViewController.m
@@ -0,0 +1,46 @@
+//
+//  MBTabBarViewController.m
+//  MusicBoxer
+//
+//  Created by 梁中豪 on 2017/9/25.
+//  Copyright © 2017年 梁中豪. All rights reserved.
+//
+
+#import "MBTabBarViewController.h"
+#import "MBfirstViewController.h"
+#import "MBsecondViewController.h"
+
+@interface MBTabBarViewController ()
+
+@end
+
+@implementation MBTabBarViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    UIViewController *vc1 = [[MBfirstViewController alloc] init];
+    UIViewController *vc2 = [[MBsecondViewController alloc] init];
+    UINavigationController *nv1 =[[UINavigationController alloc] initWithRootViewController:vc1];
+    UINavigationController *nv2 =[[UINavigationController alloc] initWithRootViewController:vc2];
+//    nv1.navigationItem.title = @"Music";
+//    nv2.navigationItem.title = @"Setting";
+    nv1.tabBarItem.title=@"音乐";
+    vc1.title = @"音乐";
+    nv2.tabBarItem.title=@"设置";
+    vc2.title = @"设置";
+    //UITabBarController *tab = [[UITabBarController alloc] init];
+    self.viewControllers = @[nv1,nv2];
+    self.delegate = self;
+}
+
+
+
+//当点击某个标签时,tabBar触发该方法
+-(void)tabBarController:(UITabBarController *)tabBarController didSelectViewController:(UIViewController *)viewController{
+    NSLog(@"%ld",tabBarController.selectedIndex);
+}
+
+
+
+
+@end
---
+++ MusicBoxer/Views/MBsecondViewController.m
@@ -0,0 +1,37 @@
+//
+//  MBsecondViewController.m
+//  MusicBoxer
+//
+//  Created by 梁中豪 on 2017/9/25.
+//  Copyright © 2017年 梁中豪. All rights reserved.
+//
+
+#import "MBsecondViewController.h"
+
+@interface MBsecondViewController ()
+
+@end
+
+@implementation MBsecondViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
